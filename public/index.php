d68920194cbc77d709cef453cf8d6215f6d210b9
Cassio Mascarenhas <csmlvx@gmail.com>
2023-04-06 14:47:51 -0300

    Adicionando as rotas no sistema

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 date_default_timezone_set('America/Sao_Paulo');
+
+
+use src\Routes\Route;
+
+$C = new Route;
+$C->get($_SERVER['REQUEST_URI']);
