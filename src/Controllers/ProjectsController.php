6c4c968758c6ceb239ef86118ff6200a35152f5a
Cassio Mascarenhas <csmlvx@gmail.com>
2023-04-06 16:47:28 -0300

    Adicionando ProjectController

--- src/Controllers/ProjectsController.php
+++ src/Controllers/ProjectsController.php
@@ -2,8 +2,29 @@
 
 namespace src\Controllers;
 
-class ProjectsController {
-    public function show() { 
-        echo "Hello mundo!";
+use src\Models\ProjectsModel;
+use src\Views\ProjectsView;
+
+class ProjectsController {    
+    public function show() {     
+        echo "oi";
+    }
+
+    public function articleShow($URI) {       
+        if (strlen($URI) <= 9) {
+            return $this->show();
+        }else {
+            $titleURI = substr($URI,10,strlen($URI));  
+            $db = new ProjectsModel();
+            $title = $db->getDataFromDB($titleURI);
+
+            if (!empty($title)) {          
+                $v = new ProjectsView();
+                $v->render($title);
+            }else{
+                $NotFound = new NotFoundController();
+                $NotFound->show();
+            }
+        }        
     }
 }
