c559ec87227711ec8e0d326698c30089f6dd728f
Cassio Mascarenhas <csmlvx@gmail.com>
2023-04-06 16:43:10 -0300

    BlogView renderizando o basico

--- src/Controllers/BlogController.php
+++ src/Controllers/BlogController.php
@@ -3,22 +3,24 @@
 namespace src\Controllers;
 
 use src\Models\BlogModel;
+use src\Views\BlogView;
 
 class BlogController {    
     public function show() {     
         echo "oi";
     }
 
-    public function articleShow($URI) {   
-       
+    public function articleShow($URI) {       
         if (strlen($URI) <= 5) {
             return $this->show();
         }else {
             $titleURI = substr($URI,6,strlen($URI));            
             $db = new BlogModel();
             $title = $db->getDataFromDB($titleURI);
-            if (!is_null($title)) {                
-                 print_r( $title);
+
+            if (!is_null($title)) {          
+                $v = new BlogView();
+                $v->render($title);
             }else{
                 $NotFound = new NotFoundController();
                 $NotFound->show();
