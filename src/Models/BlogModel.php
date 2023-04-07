a0145598b6640706352207b6560178b1887113b1
Cassio Mascarenhas <csmlvx@gmail.com>
2023-04-07 19:15:11 -0300

    Add algoritmo de paginação para blog

--- src/Models/BlogModel.php
+++ src/Models/BlogModel.php
@@ -6,8 +6,17 @@ use src\Models\DB;
 use src\Helpers\LogHelper;
 use \PDO;
 
-class BlogModel  {       
-    public function getTitleFromDB($link){
+class BlogModel  {      
+  /*
+    `title` 
+    `img` 
+    `resumo` 
+    `especial` 
+    `visitas`
+    `link` 
+    `tag`  
+  */
+    public function getLink($link){
         try {
           $db = new DB();
           $uri = $db->select('SELECT * FROM blogtable WHERE link = :link', [':link' => $link])[0];
@@ -17,7 +26,7 @@ class BlogModel  {
         }
     }
 
-    public function getDataFromDB($link){
+    public function getData($link){
       try {
         $db = new DB();
         return $db->select('SELECT * FROM blogtable WHERE link = :link', [':link' => $link]);
@@ -25,4 +34,54 @@ class BlogModel  {
         LogHelper::log($e);
       }
     }
+
+    public function getAll($page){
+      try {
+        $db = new DB();
+        $data = $db->select('SELECT * FROM blogtable ORDER BY Created_at DESC LIMIT 10 OFFSET ' . $page);
+        return $data;
+      } catch (\Throwable $e) {
+        LogHelper::log($e);
+      }
+    }
+    public function getTotal(){
+      try {
+        $db = new DB();
+        $data = $db->select('SELECT COUNT(*) as num_posts FROM blogtable');
+        return $data;
+      } catch (\Throwable $e) {
+        LogHelper::log($e);
+      }
+    }
+
+    public function getTags(){
+      try {
+        $db = new DB();
+        $data = $db->select('SELECT tag, COUNT(*) AS num_posts FROM blogtable GROUP BY tag;');
+        return $data;
+      } catch (\Throwable $e) {
+        LogHelper::log($e);
+      }
+    }
+
+    public function getPostByTag($tag){
+      try {
+        $db = new DB();
+        $data = $db->select('SELECT * FROM blogtable WHERE tag = :tag', [':tag' => $tag]);
+        return $data;
+      } catch (\Throwable $e) {
+        LogHelper::log($e);
+      }
+    }
+
+    public function search($keyWord){
+      try {
+        $db = new DB();
+        $data = $db->select('SELECT * FROM blogtable WHERE title LIKE :keyWord OR resumo LIKE :keyWord OR tag LIKE :keyWord', [':keyWord' => '%'.$keyWord.'%']);
+        return $data;
+      } catch (\Throwable $e) {
+        LogHelper::log($e);
+      }
+    }
 }
+
--- src/Views/BlogView.php
+++ src/Views/BlogView.php
@@ -8,4 +8,23 @@ class BlogView extends View{
         echo"<img src=" . $args[0]['img'] . " alt='capivarias' />";
         var_dump($args);
     }
+
+    public function pagination($total, $page){
+       $total = ceil($total/10); 
+       if ($page != 0) {       
+        if (($page-1) == 0) {
+            $text = '/blog';
+        }else{            
+            $text = "/blog/".($page-1);
+        }
+        echo "<a href='".$text."'>Anterior</a>" ;
+       }
+       for ($i=0; $i < $total; $i++) { 
+        $v = $i +1 ;
+        echo "<a href='/blog/$v'>$v</a>" ;
+       }  
+       if ($page != $total) {               
+        echo "<a href='/blog/".($page+1)."'>Proxima</a>" ;
+       }     
+    }
 }
